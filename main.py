2a77a4dc09a2e59dda63276af2b3d760742d4b79
blageo <blageo@proton.me>
2025-03-02 00:54:20 -0500

    added book report formatting

--- main.py
+++ main.py
@@ -9,12 +9,24 @@ def get_book_test(path):
 
     return contents
 
+
 def main():
     path_to_book="books/frankenstein.txt"
     frankenstein_text=get_book_test(path_to_book)
+    num_words = get_num_words(frankenstein_text)
+    char_dict = get_num_char(frankenstein_text)
+    sorted_dict = dict(sorted(char_dict.items(), key = lambda item: item[1], reverse = True))
 
-    print(f"{get_num_words(frankenstein_text)} words found in the document")
-    print(f"{get_num_char(frankenstein_text)}")
-
+    # Print report
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path_to_book}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for i in sorted_dict:
+        if i.isalpha():
+            print(f"{i}: {sorted_dict[i]}")
+    print("============= END ===============")
+    
 
 main()
